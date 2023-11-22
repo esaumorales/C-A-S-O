eebeb5fb4f0693b6d2cb0345d80de035ada1b6c9
esaumorales <102999530+esaumorales@users.noreply.github.com>
2023-11-22 15:53:57 -0500

    Add files via upload

---
+++ Caso/src/main/java/com/example/demo/commons/GlobalConstants.java
@@ -0,0 +1,21 @@
+package com.example.demo.commons;
+
+public class GlobalConstants {
+	
+	
+	/* APIS */
+
+	public static final String  	API_LIBROS			=	"/libro";
+	public static final String  	API_ALQUILERES		=	"/alquiler";
+	public static final String  	API_AUTORES			=	"/autor";
+	public static final String  	API_CATEGORIAS	    =	"/categoria";
+	public static final String  	API_EDITORIALES		=	"/editorial";
+	public static final String  	API_LECTORES	    =	"/lector";
+
+
+	
+
+	
+	public static final Integer  	COD_CONSULTA_EXITO		=	1;	
+	public static final String  	MSG_CONSULTA_EXITO		=	"La consulta fue realizada exitosamente";
+}
